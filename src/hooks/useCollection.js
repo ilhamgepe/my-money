824262eae8a9ce107659191aebf9da15a530da57
ilhamgepe <gepedevelop@gmail.com>
2022-05-31 22:04:10 +0700

    explain why we use useRef for _query

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,14 +1,22 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 //firestore
 import { projectFireStore } from '../firebase/config'
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
     const [documents, setDocuments] = useState(null)
     const [error, setError] = useState(null)
+    //kenapa kita gunakan useRef? karena _query/query itu kita bungkus dalam array
+    //  ex:["uid","==",user.uid,], jadi ketika apapun komponen menggunakan useCollection dan menggunakan parameter ke 2 yaitu _query. component akan merender terus karna ITU ARRAY, array akan selalu di taro di random memori, jd ketika render pertama pasti terlihat berbeda walau isinya sama, mangkanya kita pake useref, useref ini bisa di buat edit DOM dan mengecek apakah isi si _query itu sama atau tidak, kalo sama jadi tetap di simpan di memori yg sama, yg mengakibatkan tidak terjadi infinity loop. mangkanya kita pakai useref dari pada langsung mengkonsumsi array dari _query 
+    const query = useRef(_query).current
 
     useEffect(() => {
-        const ref = projectFireStore.collection(collection)
+        let ref = projectFireStore.collection(collection)
+
+        if (query) {
+            ref = ref.where(...query)
+        }
+
         const unsubscribe = ref.onSnapshot(snapshot => {
             // cara copilot
             const documents = snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
@@ -33,7 +41,7 @@ export const useCollection = (collection) => {
         return () => {
             unsubscribe()
         }
-    }, [collection])
+    }, [collection, query])
 
     return { documents, error }
 }
